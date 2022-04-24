30069ade388987ec164ae27a836f3fe704d0800e
asaikarthikeya <a.saikarthikeya@gmail.com>
2022-04-25 01:10:12 +0530

    mainViewModel - the connector connects service and view model. viewmodel later talks with activity.

--- app/src/main/java/com/plcoding/spotifycloneyt/di/AppModule.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/di/AppModule.kt
@@ -5,6 +5,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.plcoding.spotifycloneyt.R
+import com.plcoding.spotifycloneyt.exoplayer.MusicServiceConnection
 //import com.plcoding.spotifycloneyt.adapters.SwipeSongAdapter
 //import com.plcoding.spotifycloneyt.exoplayer.MusicServiceConnection
 import dagger.Module
@@ -18,12 +19,12 @@ import javax.inject.Singleton
 @InstallIn(ApplicationComponent::class)
 object AppModule {
 
-//    @Singleton
-//    @Provides
-//    fun provideMusicServiceConnection(
-//        @ApplicationContext context: Context
-//    ) = MusicServiceConnection(context)
-//
+    @Singleton
+    @Provides
+    fun provideMusicServiceConnection(
+        @ApplicationContext context: Context
+    ) = MusicServiceConnection(context)
+
 //    @Singleton
 //    @Provides
 //    fun provideSwipeSongAdapter() = SwipeSongAdapter()
